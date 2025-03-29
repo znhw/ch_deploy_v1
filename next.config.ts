12cac4ae1a0a80e582843d46a56bed234f090dd4
znhw <zenhew@yahoo.com>
2025-03-29 16:08:19 +0800

    add url setting in config file

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // Forces static HTML export
   trailingSlash: true, // Ensures proper routing
+  basePath: '', // Remove the basePath if using a custom domain
+  assetPrefix: '', // Remove assetPrefix if using a custom domain
+
 };
 
 export default nextConfig;
